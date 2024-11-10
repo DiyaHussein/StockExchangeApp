b9b8828eacb2b87eb16693d756ccf0585d4e4cf4
dpupt <popescudavidalexandruemanuel@gmail.com>
2024-11-10 22:59:44 +0200

    added debug code in Main: simulate a Stock Exchange with 5 users and 10 random trades

--- src/main/java/com/stockexchange/stockexchange/StockExchangeAppApplication.java
+++ src/main/java/com/stockexchange/stockexchange/StockExchangeAppApplication.java
@@ -9,8 +9,29 @@ public class StockExchangeAppApplication {
     public static void main(String[] args) {
         //SpringApplication.run(StockExchangeAppApplication.class, args);
 
-        StockExchangeService stockExchangeService = new StockExchangeService();
-        stockExchangeService.setUpRandomStockExchange();
+        int nOfRandomOrders = 10;
+        int nOfRandomUsers = 5;
+
+        UserDatabase userDatabase = new UserDatabase();
+        StockMarket stockMarket = new StockMarket(userDatabase);
+
+        StockExchangeService stockExchangeService = new StockExchangeService(userDatabase, stockMarket);
+
+        OrderMatcher orderMatcher = new OrderMatcher(stockMarket);
+
+        Thread orderMatcherThread = new Thread(orderMatcher);
+        orderMatcherThread.start();
+
+        userDatabase.populateRandomUsers(nOfRandomUsers);
+        stockMarket.populateRandomOrders(nOfRandomOrders);
+
+        // add a shutdown hook to stop the OrderMatcher gracefully
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            orderMatcherThread.interrupt();
+            System.out.println("OrderMatcher stopped.");
+        }));
+
+        //stockExchangeService.setUpRandomStockExchange();
     }
 
 }
